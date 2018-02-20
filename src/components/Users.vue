5e77eed3e5169de02903c22c2aa610fecb46f960
antarus66 <antarus66@gmail.com>
2018-02-20 23:22:24 +0200

    Added users component with simple users list rendering

---
+++ src/components/Users.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h3>Users list</h3>
+        <ul>
+            <li v-for="user in users">{{ user.username }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        components: {
+
+        },
+
+        data () {
+            return {
+                users: []
+            }
+        },
+
+        methods: {
+            fetchUsers() {
+                fetch("https://jsonplaceholder.typicode.com/users")
+                    .then(response => response.json())
+                    .then(users => {
+                        this.users = users;
+                    });
+            }
+        },
+
+        created() {
+            this.fetchUsers();
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
